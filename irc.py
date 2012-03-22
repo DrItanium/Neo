eda86299a1cae8b326a6cf1e642ba18fead56e8b
Marcus Hildum <mjhildum@csupomona.edu>
2012-03-22 14:22:52 -0700

    Cleaned up some non modular variables and added config file

--- irc.py
+++ irc.py
@@ -1,24 +1,30 @@
-import socket, string, random, time
+import socket, string, random, time, ConfigParser
 from threading import Timer
 from loader import Loader
 
 class Irc:
-	server = 'irc.freenode.net'
-	port = 6667
-	nick = 'girldius'
-	username = 'girldius'
-	password = None
-	realname = 'gradiusbot'
-	hostname = 'gibson'
-	servername = 'gibson'
-	channel = '#testgradius'
-	owner = 'gradius'
-	silent = False
-	delay = False
-	delayTime = 5
+	#Parse Configuration File
+	config = ConfigParser.RawConfigParser()
+	config.read('config.cfg')
+
+	#Obtain Configuration Vaules
+	port 		= config.get('Settings', 'port')
+	nick 		= config.get('Settings', 'nick')
+	username 		= config.get('Settings', 'username')
+	password 		= config.get('Settings', 'password')
+	realname 		= config.get('Settings', 'realname')
+	hostname 		= config.get('Settings', 'hostname')
+	servername 	= config.get('Settings', 'servername')
+	channel 		= config.get('Settings', 'channel')
+	owner 		= config.get('Settings', 'owner')
+	silent 		= config.get('Settings', 'silent')
+	delay 		= config.get('Settings', 'delay')
+	delayTime 	= config.get('Settings', 'delayTime')
+
+	#setup irc socket
 	irc = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 	
-	# init the class
+	#init the class
 	def __init__(self, datloader):
 		self.load = datloader
 		
@@ -53,25 +59,25 @@ class Irc:
 		
 		if message.find(self.nick+':') != -1 and len(message.split()) > 1:
 
-			if ( message.split()[1] == 'loader' and sender == 'gradius' ):
+			if ( message.split()[1] == 'loader' and sender == owner ):
 				if ( message.split()[2] == 'load' ):
-					self.saychan(self.load.loadMod(message.split()[3]),'#testgradius')
+					self.saychan(self.load.loadMod(message.split()[3]),channel)
 				elif ( message.split()[2] == 'reload' ):
-					self.saychan(self.load.reloadMod(message.split()[3]),'#testgradius')
+					self.saychan(self.load.reloadMod(message.split()[3]),channel)
 				elif ( message.split()[2] == 'run' ):
-					self.saychan(self.load.runMethod(message.split()[3],message.split()[4],None),'#testgradius')
+					self.saychan(self.load.runMethod(message.split()[3],message.split()[4],None),channel)
 				elif ( message.split()[2] == 'showmethods' ):
-					self.saychan(self.load.showMethods(''),'#testgradius')
+					self.saychan(self.load.showMethods(''),channel)
 
-			elif ( self.load.modules.has_key(message.split()[1]) and sender == 'gradius' ):
+			elif ( self.load.modules.has_key(message.split()[1]) and sender == owner ):
 				if ( message.split()[2] != None ):
-					self.saychan(self.load.runMethod(message.split()[1],message.split()[2],message.split()[3:len(message.split())]),'#testgradius')
+					self.saychan(self.load.runMethod(message.split()[1],message.split()[2],message.split()[3:len(message.split())]),channel)
 				else:
-					self.saychan(self.load.runMethod(message.split()[1],message.split()[2],None),'#testgradius')
-					## self.saychan("yeah yeah I know some of these words",'#testgradius')
+					self.saychan(self.load.runMethod(message.split()[1],message.split()[2],None),channel)
+					## self.saychan("yeah yeah I know some of these words",channel)
 				
 			else:
-				self.saychan(message.split()[1],'#testgradius')
+				self.saychan(message.split()[1],channel)
 
 
 	def startIrc(self):
