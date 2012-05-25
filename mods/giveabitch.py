7e79af00a343b83845b3e4ee389e0b76f3c5be5e
Jared Rathbun <gradiuscypher@gmail.com>
2012-05-24 23:58:21 -0700

    Adding the best module ever.

---
+++ mods/giveabitch.py
@@ -0,0 +1,20 @@
+## This is an example file for how all IRC modules should be
+
+import random
+## Name of the module, to be returned in desc()
+name = "giveabitch.py"
+
+## What the module should return when processing text from IRC
+## Gets inp from IRC, processes it, and returns the 
+def run(inp,sender):
+
+	words = [line.strip() for line in open('items.dic')]
+	gift = random.choice(words).upper()
+	if ( inp == "what do you give a bitch?" ):
+		return 'GIVE THE BITCH A ' + gift + '. BITCHES LOVE ' + gift + 'S.'
+	else:
+		return ""
+
+## Returns a description of the module including the name at the top
+def desc():
+	return name + ":Tells you what to give a bitch."
